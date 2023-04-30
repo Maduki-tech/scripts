a29176c3d772a909e9a04e2f7431787226519124
Maduki-tech <maduki.business@gmail.com>
2023-04-30 12:34:55 +0200

    added fast folder creation

---
+++ fp.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+options=("work" "personal" "other")
+PS3="Select folder: "
+
+select folder_name in "${options[@]}"; do
+    if [[ -z $folder_name ]]; then
+        echo "Invalid selection."
+        exit 1
+    fi
+    break
+done
+
+read -p "Enter subfolder name: " subfolder_name
+
+if [[ -z $subfolder_name ]]; then
+    echo "Subfolder name cannot be empty."
+    exit 1
+fi
+
+mkdir "$HOME/$folder_name/$subfolder_name"
+
+if [[ $? -ne 0 ]]; then
+    echo "Error creating subfolder '$subfolder_name' in '$folder_name'."
+    exit 1
+fi
+
+echo "Subfolder '$subfolder_name' created successfully in '$folder_name' folder of your home directory."
+
+selected="$HOME/$folder_name/$subfolder_name"
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t $selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
+
